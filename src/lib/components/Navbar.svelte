cb8194f3357d5be8fd2f7d63b3e2724b211d78b3
Gyeony95 <rlg1133@naver.com>
2024-05-10 18:57:50 +0900

    feat : 화면 사이즈에 따라 갭 크기 수정

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Cart from '$lib/components/Cart.svelte';
-	import { cartItemsStore } from '$lib/stores';
 	import { onMount } from 'svelte';
 	import '$lib/model/MenuModel.ts';
 	import { browser } from '$app/environment';
@@ -13,8 +12,6 @@
 			menus = await response.json();
 		}
 	});
-
-	let cartOpened = false;
 </script>
 
 <nav class="sticky top-0 z-10 bg-[#282828] text-white">
@@ -25,7 +22,7 @@
 			<div>
 				<a class="mx-5 font-extrabold text-xl" href="/">삼원파워감속기</a>
 			</div>
-			<div class="flex items-center gap-6 m-auto">
+			<div class="flex items-center gap-4 sm:gap-6 md:gap-8 lg:gap-12 xl:gap-16 m-auto">
 				{#each menus as menu}
 					<div class="menu">
 						<a class="menu-item hover:underline underline-offset-4" href={menu.link}>{menu.title}</a
@@ -49,54 +46,9 @@
 					</div>
 				{/each}
 			</div>
-			<div class="flex mx-5 gap-6">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-					/>
-				</svg>
-				<button class="relative" aria-label="Cart" on:click={() => (cartOpened = !cartOpened)}>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-						/>
-					</svg>
-					{#if $cartItemsStore.length !== 0}
-						<span class="absolute inset-0 object-right-top ml-3">
-							<div
-								class="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white"
-							>
-								{$cartItemsStore.length}
-							</div>
-						</span>
-					{/if}
-				</button>
-			</div>
 		</div>
 	</div>
 </nav>
-
-{#if cartOpened}
-	<Cart bind:cartOpened />
-{/if}
 
 <style>
 	/* 메뉴 스타일링 */
@@ -111,7 +63,7 @@
 		display: none;
 		position: absolute;
 		background-color: #f9f9f9;
-		min-width: 160px;
+		min-width: 180px;
 		box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
 		z-index: 1;
 	}
