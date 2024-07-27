fb906c6333ff51314db35328108f5f312a888a05
Gyeony95 <rlg1133@naver.com>
2024-07-27 15:52:33 +0900

    fix : 주석 수정 및 버튼명 수정

--- src/routes/draw/+page.svelte
+++ src/routes/draw/+page.svelte
@@ -8,25 +8,17 @@
 	let monoisotopicWeight = writable('');
 
 	onMount(() => {
-		// changes the default JMol color of hydrogen to black so it appears on white backgrounds
+		// 기초 셋팅  ex. https://web.chemdoodle.com/tutorial/2d-structure-canvases/sketcher-canvas 참고
 		ChemDoodle.ELEMENT['H'].jmolColor = 'black';
-		// darkens the default JMol color of sulfur so it appears on white backgrounds
 		ChemDoodle.ELEMENT['S'].jmolColor = '#B9A130';
-		// initializes the SketcherCanvas
 		sketcher = new ChemDoodle.SketcherCanvas('sketcher', 500, 300, {
 			useServices: true,
-			oneMolecule: true
+			oneMolecule: true // 분자구조 한개만 그리게 처리
 		});
-		// sets terminal carbon labels to display
 		sketcher.styles.atoms_displayTerminalCarbonLabels_2D = true;
-		// sets atom labels to be colored by JMol colors, which are easy to recognize
 		sketcher.styles.atoms_useJMOLColors = true;
-		// enables overlap clear widths, so that some depth is introduced to overlapping bonds
 		sketcher.styles.bonds_clearOverlaps_2D = true;
-		// sets the shape color to improve contrast when drawing figures
 		sketcher.styles.shapes_color = 'c10000';
-		// because we do not load any content, we need to repaint the sketcher, otherwise we would just see an empty area with the toolbar
-		// however, you can instead use one of the Canvas.load... functions to pre-populate the canvas with content, then you don't need to call repaint
 		sketcher.repaint();
 	});
 
@@ -41,72 +33,11 @@
 		console.log('Molecular Formula:', molecule.getMolecularFormula());
 		console.log('Molecular Weight:', molecule.getMonoisotopicMass());
 		console.log('ghghgh ' + asString);
-		// // 프로판 구조 추출 및 화학식 생성
-		// let atomCounts = countAtoms(mol);
-		// atomCounts = addHydrogenAtoms(atomCounts, mol);
-		// const molecularFormulaValue = createMolecularFormula(atomCounts);
-
 		console.log(molecule.getMolecularFormula()); // "C5H12"
+
 		molecularFormula.set(molecule.getMolecularFormula());
 		monoisotopicWeight.set(molecule.getMonoisotopicMass().toString());
 	}
-
-	// // 원자 개수를 세는 함수
-	// function countAtoms(json: any) {
-	// 	const atomCounts: { [key: string]: number } = {};
-
-	// 	json.atoms.forEach((atom: any) => {
-	// 		const label = atom.label;
-	// 		if (atomCounts[label]) {
-	// 			atomCounts[label]++;
-	// 		} else {
-	// 			atomCounts[label] = 1;
-	// 		}
-	// 	});
-
-	// 	return atomCounts;
-	// }
-
-	// // 수소 원자 추가 함수
-	// function addHydrogenAtoms(atomCounts: { [key: string]: number }, json: any) {
-	// 	// 각 탄소 원자가 4개의 결합을 갖도록 수소 원자를 추가합니다.
-	// 	let carbonBonds: { [key: number]: number } = {};
-	// 	json.bonds.forEach((bond: any) => {
-	// 		if (bond.a1.label === 'C') {
-	// 			if (carbonBonds[bond.a1.pid]) {
-	// 				carbonBonds[bond.a1.pid]++;
-	// 			} else {
-	// 				carbonBonds[bond.a1.pid] = 1;
-	// 			}
-	// 		}
-	// 		if (bond.a2.label === 'C') {
-	// 			if (carbonBonds[bond.a2.pid]) {
-	// 				carbonBonds[bond.a2.pid]++;
-	// 			} else {
-	// 				carbonBonds[bond.a2.pid] = 1;
-	// 			}
-	// 		}
-	// 	});
-
-	// 	// 탄소 원자마다 부족한 결합 수만큼 수소 원자를 추가합니다.
-	// 	let hydrogenCount = 0;
-	// 	for (const pid in carbonBonds) {
-	// 		hydrogenCount += 4 - carbonBonds[pid];
-	// 	}
-	// 	atomCounts['H'] = hydrogenCount;
-	// 	return atomCounts;
-	// }
-
-	// // 화학식 문자열을 만드는 함수
-	// function createMolecularFormula(atomCounts: { [key: string]: number }) {
-	// 	let formula = '';
-
-	// 	for (const atom in atomCounts) {
-	// 		formula += atom + atomCounts[atom];
-	// 	}
-
-	// 	return formula;
-	// }
 </script>
 
 <svelte:head>
@@ -120,7 +51,7 @@
 <main>
 	<h1>Chemical Draw Canvas</h1>
 	<canvas id="sketcher" width="500" height="500" />
-	<button on:click={onTapButton}>Extract SMILES</button>
+	<button on:click={onTapButton}>Calulate Chem</button>
 	{#if molecularFormula}
 		<p>molecularFormula : {$molecularFormula}</p>
 	{/if}
