51b8608a28aebff5f887241ef817f7e54c39bb37
Gyeony95 <rlg1133@naver.com>
2024-07-27 16:07:18 +0900

    feat : 계산된 데이터 저장하고 리스트로 보여주는 로직 추가

--- src/routes/draw/+page.svelte
+++ src/routes/draw/+page.svelte
@@ -6,9 +6,11 @@
 	let sketcher: ChemDoodle.SketcherCanvas;
 	let molecularFormula = writable('');
 	let monoisotopicWeight = writable('');
+	let moleculeJson = writable('');
+	let savedData = writable([]);
 
 	onMount(() => {
-		// 기초 셋팅  ex. https://web.chemdoodle.com/tutorial/2d-structure-canvases/sketcher-canvas 참고
+		// 기초 셋팅
 		ChemDoodle.ELEMENT['H'].jmolColor = 'black';
 		ChemDoodle.ELEMENT['S'].jmolColor = '#B9A130';
 		sketcher = new ChemDoodle.SketcherCanvas('sketcher', 500, 300, {
@@ -20,6 +22,9 @@
 		sketcher.styles.bonds_clearOverlaps_2D = true;
 		sketcher.styles.shapes_color = 'c10000';
 		sketcher.repaint();
+
+		// 저장된 데이터 불러오기
+		loadSavedData();
 	});
 
 	function onTapButton() {
@@ -27,16 +32,43 @@
 		let dummy = new ChemDoodle.io.JSONInterpreter().molTo(mol);
 		let obj = new ChemDoodle.io.JSONInterpreter().molFrom(dummy);
 		let asString = JSON.stringify(obj);
+		moleculeJson.set(asString);
 		const molecule = Molecule.fromJson(asString);
-
-		console.log('Molecule JSON:', molecule.getMoleculeJson());
-		console.log('Molecular Formula:', molecule.getMolecularFormula());
-		console.log('Molecular Weight:', molecule.getMonoisotopicMass());
-		console.log('ghghgh ' + asString);
-		console.log(molecule.getMolecularFormula()); // "C5H12"
 
 		molecularFormula.set(molecule.getMolecularFormula());
 		monoisotopicWeight.set(molecule.getMonoisotopicMass().toString());
+	}
+
+	/// 계산된 데이터 로컬 스토리지에 저장
+	function saveData() {
+		// 계산된게 없으면 리턴 처리
+		if ($moleculeJson == '') return alert('Please Input Data');
+
+		/// 계산된 데이터를 하나의 맵으로 만들어 저장
+		const dataSet = {
+			moleculeJson: $moleculeJson,
+			molecularFormula: $molecularFormula,
+			monoisotopicWeight: $monoisotopicWeight
+		};
+		// 기존 데이터 있으면 불러옴
+		let storedData = JSON.parse(localStorage.getItem('moleculeData') || '[]');
+		storedData.push(dataSet);
+		localStorage.setItem('moleculeData', JSON.stringify(storedData));
+		loadSavedData();
+	}
+
+	/// 저장된 데이터 불러오기
+	function loadSavedData() {
+		let storedData = JSON.parse(localStorage.getItem('moleculeData') || '[]');
+		savedData.set(storedData);
+	}
+
+	/// 리스트에서 아이템 삭제
+	function deleteData(index) {
+		let storedData = JSON.parse(localStorage.getItem('moleculeData') || '[]');
+		storedData.splice(index, 1);
+		localStorage.setItem('moleculeData', JSON.stringify(storedData));
+		loadSavedData();
 	}
 </script>
 
@@ -51,13 +83,27 @@
 <main>
 	<h1>Chemical Draw Canvas</h1>
 	<canvas id="sketcher" width="500" height="500" />
-	<button on:click={onTapButton}>Calulate Chem</button>
-	{#if molecularFormula}
-		<p>molecularFormula : {$molecularFormula}</p>
+	<button on:click={onTapButton}>Calculate Chem</button>
+	{#if $molecularFormula}
+		<p class="result">Molecular Formula: {$molecularFormula}</p>
 	{/if}
-	{#if monoisotopicWeight}
-		<p>monoisotopicWeight : {$monoisotopicWeight}</p>
+	{#if $monoisotopicWeight}
+		<p class="result">Monoisotopic Weight: {$monoisotopicWeight}</p>
 	{/if}
+	<button on:click={saveData}>Save Data</button>
+
+	<h2>Saved Molecule Data</h2>
+	<ul>
+		{#each $savedData as data, index}
+			<li>
+				<div class="data-item">
+					<p>Molecular Formula: {data.molecularFormula}</p>
+					<p>Monoisotopic Weight: {data.monoisotopicWeight}</p>
+					<button on:click={() => deleteData(index)}>X</button>
+				</div>
+			</li>
+		{/each}
+	</ul>
 </main>
 
 <style>
@@ -73,9 +119,9 @@
 		margin: 40px auto;
 	}
 
-	h1 {
+	h1,
+	h2 {
 		font-family: 'Arial', sans-serif;
-		font-size: 24px;
 		color: #333;
 		margin-bottom: 20px;
 	}
@@ -107,9 +153,44 @@
 		background-color: #004494;
 	}
 
-	p {
+	p.result {
 		margin-top: 20px;
 		font-size: 18px;
 		color: #333;
+		background-color: #e9e9e9;
+		padding: 10px;
+		border-radius: 8px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+		width: 100%;
+	}
+
+	li {
+		margin-top: 10px;
+		background-color: #fff;
+		padding: 15px;
+		border-radius: 8px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.data-item {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.data-item p {
+		margin: 0;
+	}
+
+	.data-item button {
+		width: 70px;
+		background-color: #ff4d4d;
 	}
 </style>
